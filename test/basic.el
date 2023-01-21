b943f8af33e0c96f6e4274be30406643884047c9
Lukas Barth <lukas.barth@kit.edu>
2023-01-21 22:02:25 +0100

    deduplicate common testing code

--- test/basic.el
+++ test/basic.el
@@ -3,45 +3,7 @@
 (require 'buttercup)
 (require 'org-incoming)
 
-(setq org-incoming--test-dir (concat default-directory "test"))
-
-(defvar org-incoming--test-base "" nil)
-
-(defun file-name-concat (lhs rhs)
-	 (concat (file-name-as-directory lhs) rhs)) 
-
-(defun org-incoming--test-absfile (relfile)
-	(file-name-concat org-incoming--test-base relfile))
-
-(defun org-incoming--test-create-dir ()
-	""
-	(setq org-incoming--test-base (make-temp-file "org-incoming-test" 't))
-	(message (format "Testdir: %s" org-incoming--test-base)))
-
-(defun org-incoming--test-cleanup-dir ()
-	""
-	(delete-directory org-incoming--test-base 't))
-
-(defun org-incoming--test-mkdir (reldir)
-	(let ((absdir (org-incoming--test-absfile reldir)))
-		(make-directory absdir 't)))
-
-(defun org-incoming--test-prepare-file (srcdstpair)
-	""
-	(let ((source (nth 0 srcdstpair))
-				(destination (nth 1 srcdstpair)))
-		(message (format "%s -> %s" source destination))
-		(let* ((testbase org-incoming--test-dir)
-					 (fullsrc (file-name-concat testbase source))
-					 (fulldst (org-incoming--test-absfile destination))
-					 (dstdir (file-name-directory fulldst)))
-			(make-directory dstdir 't)
-			(copy-file fullsrc fulldst))))
-
-
-(defun org-incoming--test-prepare-files (srcdstlst)
-	""
-	(-each srcdstlst 'org-incoming--test-prepare-file))
+(load "./test/test-common.el")
 
 (describe "Process a single file with default settings"
 					:var ((org-incoming-dirs `((:source ,(org-incoming--test-absfile "incoming") :target ,(org-incoming--test-absfile "processed")))))
